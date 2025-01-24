ac275289713277025ace223c1560cd38150b08f7
Anir Jung Thapa <134839446+Aneer-Thapa1@users.noreply.github.com>
2025-01-24 23:08:01 +0545

    paths setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Landing from "./pages/Landing";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { BrowserRouter } from "react-router";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      {" "}
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
