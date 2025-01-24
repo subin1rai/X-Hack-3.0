c968bcd082a333cae09106aa52f12510cd270c7d
subin1rai <bantawasubin@gmail.com>
2025-01-25 03:57:09 +0545

    landing page all

--- client/src/constants/images.jsx
+++ client/src/constants/images.jsx
@@ -1,5 +1,14 @@
 import register from "../assets/farmer.png";
+import plantd from "../assets/plantd.png";
+import shopplant from "../assets/shopplant.png";
+import farmerMan from "../assets/farmerMan.png";
+import seller from "../assets/seller.png";
 
 export default {
   register,
+  plantd,
+  shopplant,
+  farmerMan,
+  seller
 };
+
